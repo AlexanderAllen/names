c6f701529a5addc1e50deb92ad97d9438660dd75
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2021-03-10 22:23:03 -0500

    Replace underscore with hyphen.

--- names.go
+++ names.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-        "fmt"
-        "math/rand"
-        "time"
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
 
-        "github.com/docker/docker/pkg/namesgenerator"
+	"github.com/docker/docker/pkg/namesgenerator"
 )
 
 func main() {
-        rand.Seed(time.Now().UnixNano())
-        fmt.Println(namesgenerator.GetRandomName(0))
+	rand.Seed(time.Now().UnixNano())
+
+	name := namesgenerator.GetRandomName(0)
+	fmt.Println(strings.Replace(name, "_", "-", 1))
 }
